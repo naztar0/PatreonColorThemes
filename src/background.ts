34464f97088e55d5d7981f73ea9060ea91881fe3
Nazar Taran <nazar.taran.id@gmail.com>
2023-09-26 16:53:16 +0200

    Add injected UI

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,8 @@
 import themesAsset from '@/assets/json/themes.json';
 import stylesAsset from '@/assets/json/styles.json';
 import excStylesAsset from '@/assets/json/exception_styles.json';
+import themeIcon from '@/assets/icons/theme.svg?raw';
+import './inject.css';
 
 let colorTheme: string | null = null;
 const defaultTheme = 'dark';
@@ -52,6 +54,54 @@ const changeTheme = () => {
     setTimeout(() => document.head.appendChild(additionalStyleElement), 100);
   }
 };
+
+const injectUI = () => {
+  const toggle = document.createElement('div');
+  toggle.id = 'pct-toggle';
+  toggle.innerHTML = `
+    <button class="pct-btn-round">${themeIcon}</button>
+  `;
+  const dropdown = document.createElement('div');
+  dropdown.classList.add('pct-dropdown-list');
+  dropdown.innerHTML = `
+    <ul>
+      <li data-theme="light">Light</li>
+      <li data-theme="lightWarm">Light Warm</li>
+      <li data-theme="dark">Dark</li>
+      <li data-theme="darkDimmed">Dark Dimmed</li>
+      <li data-theme="darkHighContrast">Dark High Contrast</li>
+      <li data-theme="darkWarm">Dark Warm</li>
+      <li data-theme="darkCool">Dark Cool</li>
+    </ul>
+  `;
+  toggle.addEventListener('click', () => {
+    dropdown.classList.toggle('show');
+    dropdown.style.top = `${toggle.getBoundingClientRect().bottom + 5}px`;
+    dropdown.style.left = `${toggle.getBoundingClientRect().left}px`;
+  });
+  dropdown.querySelectorAll('li').forEach((li) => {
+    li.addEventListener('click', () => {
+      const theme = li.getAttribute('data-theme');
+      if (theme) {
+        colorTheme = theme;
+        changeTheme();
+        chrome.storage.sync.set({ color_theme: theme }).then();
+      }
+    });
+  });
+  const parent = document.querySelector('nav[aria-label="Patron navigation"] > div:first-child');
+  if (parent) {
+    parent.appendChild(toggle);
+    parent.appendChild(dropdown);
+  }
+  document.addEventListener('click', (e) => {
+    if (!toggle.contains(e.target as Node) && !dropdown.contains(e.target as Node)) {
+      dropdown.classList.remove('show');
+    }
+  });
+};
+
+injectUI();
 
 chrome.storage.sync.get(['color_theme'], (result) => {
   if (result.color_theme) {
